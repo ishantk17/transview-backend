9161e41dd57637f8bb3638e86279f1fead167364
ishantk17 <ishantkumar85627@gmail.com>
2024-02-27 21:14:30 +0530

    done

--- Server.js
+++ Server.js
@@ -2,7 +2,9 @@ const express=require('express')
 const app=express();
 const connectDb = require('./config/db')
 connectDb();
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 require("./models/Users")
 require("./models/GreenForm")
--- models/Users.js
+++ models/Users.js
@@ -1,32 +1,28 @@
 const mongoose=require('mongoose');
 
 const UserSchema=mongoose.Schema({
+   
     UserType:{
         type:String,
-        required:true
     },
     username:{
         type:String,
-        required:true
+       
     },
     gst:{
         type:String
     },
     pan:{
         type:String,
-        required:true
     },
     aadhar:{
-        type:Number
+        type:String
     },
     Email:{
         type: String,
-        unique: true,
-        required: 'Email address is required',
     },
     Phone:{
-        type:Number,
-        required:'Mobile Number is required'
+        type:String,
     }
 })
 
