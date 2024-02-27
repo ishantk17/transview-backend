ceced43d8efb34b109a1d4e86e3264d439981ca2
ishantk17 <ishantkumar85627@gmail.com>
2024-02-27 21:35:33 +0530

    final

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://TSIL:CzDZJdXu9WoT73UL@cluster0.5acaggl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://TSIL:CzDZJdXu9WoT73UL@cluster0.5acaggl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0?directConnection=true"
     );
     console.log("mongo db connected");
   } catch (error) {
